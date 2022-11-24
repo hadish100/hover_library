c7c5e19f3e5491896521dd2d411f33ecd39f5bdb
Pouya Hamraz <110659145+hadish100@users.noreply.github.com>
2022-11-24 10:05:17 -0800

    Add files via upload

---
+++ js/hl.js
@@ -0,0 +1,17 @@
+function hl_popup_open()
+{
+$(".hl_popup_flexor").css("display","flex");
+$(".hl_popup_main").css("animation","fadeInDown 0.5s 1");
+//$(".hl_popup_main").css("animation","jackInTheBox 1s 1");
+$(".hl_popup_bg").css("animation","hl_popup_opacity 0.5s 1");
+$(".hl_popup_bg").css("opacity","0.5");
+}
+function hl_popup_close()
+{
+$(".hl_popup_bg").css("opacity","0");
+$(".hl_popup_main").css("animation","fadeOutDown 0.5s 1");
+//$(".hl_popup_main").css("animation","fadeOut 0.5s 1");
+setTimeout(function(){
+$(".hl_popup_flexor").css("display","none");		
+},480);
+}
